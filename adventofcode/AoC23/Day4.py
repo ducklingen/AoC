306152ea1433c5e025a6dfcbd3c36c7b41716ac2
Andreas Midjord <amid@baselifescience.com>
2023-12-06 07:19:09 +0100

    Refactoring solution for Day 4

--- adventofcode/AoC23/Day4.py
+++ adventofcode/AoC23/Day4.py
@@ -2,25 +2,47 @@ from collections import Counter
 
 from adventofcode.helpers import AoCHelper
 
-input = AoCHelper.read_input_lines("AoC23/Inputs/Day4/inputs1.txt")
 
-cards = [i[8:] for i in input]
-
-result = 0
-counter = Counter({i: 1 for i in range(len(input))})
-for idx, c in enumerate(cards):
-    winners, mine = c.split("|")
+def identify_matches(card: str) -> int:
+    winners, mine = card.split("|")
     winning_numbers = AoCHelper.extract_numbers_from_line(winners)
     my_numbers = AoCHelper.extract_numbers_from_line(mine)
 
-    matches = len(set(winning_numbers).intersection(my_numbers))
-    if matches > 0:
-        result += 2 ** (matches - 1)
+    return len(set(winning_numbers).intersection(my_numbers))
 
-    counter += {idx + i + 1: counter[idx] for i in range(matches)}
 
-assert result == 23847
-print(f"Part 1: {result}")
-result2 = sum(counter.values())
-assert result2 == 8570000
-print(f"Part 2: {result2}")
+def parse_cards(filename: str) -> list[str]:
+    input = AoCHelper.read_input_lines(f"AoC23/Inputs/Day4/{filename}.txt")
+
+    return [i[8:] for i in input]
+
+
+def compute_one(filename: str) -> int:
+    result = 0
+
+    for c in parse_cards(filename):
+        matches = identify_matches(c)
+        if matches > 0:
+            result += 2 ** (matches - 1)
+
+    return result
+
+
+def compute_two(filename: str) -> int:
+    cards = parse_cards(filename)
+    counter = Counter({i: 1 for i in range(len(cards))})
+    for idx, c in enumerate(cards):
+        matches = identify_matches(c)
+
+        counter += {idx + i + 1: counter[idx] for i in range(matches)}
+
+    return sum(counter.values())
+
+
+if __name__ == "__main__":
+    result = compute_one("inputs1")
+    assert result == 23847
+    print(f"Part 1: {result}")
+    result2 = compute_two("inputs1")
+    assert result2 == 8570000
+    print(f"Part 2: {result2}")
