3ce815701318819202830c49b7e0f641a762ae0f
Andreas Midjord <amid@baselifescience.com>
2023-12-15 09:52:20 +0100

    Initial solution for part 2 day 15

--- adventofcode/AoC23/Day15.py
+++ adventofcode/AoC23/Day15.py
@@ -1,3 +1,5 @@
+import re
+
 from adventofcode.helpers.AoCHelper import read_input_lines
 
 input = read_input_lines("AoC23/Inputs/Day15/inputs.txt")[0]
@@ -20,3 +22,28 @@ for init in init_sequence:
     result += compute_hash(init)
 
 print(result)
+
+boxes = [{} for _ in range(256)]
+for init in init_sequence:
+    op_match = re.search(r"=|-", init)
+    op_idx = op_match.start()
+    label = init[:op_idx]
+    operation = init[op_idx]
+
+    label_hash = compute_hash(label)
+
+    if operation == "-":
+        if label in boxes[label_hash].keys():
+            del boxes[label_hash][label]
+    else:
+        focal_lenght = init[op_idx + 1]
+        boxes[label_hash][label] = focal_lenght
+
+result2 = 0
+for idx, box in enumerate(boxes):
+    for idy, label in enumerate(box.keys()):
+        score = (idx + 1) * (idy + 1) * int(box[label])
+        print(score)
+        result2 += score
+
+print(result2)
