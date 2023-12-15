63e36302390f6f22c21330b09d093e608c4c2961
Andreas Midjord <amid@baselifescience.com>
2023-12-15 09:57:06 +0100

    Refactoring of day 15

--- adventofcode/AoC23/Day15.py
+++ adventofcode/AoC23/Day15.py
@@ -2,11 +2,6 @@ import re
 
 from adventofcode.helpers.AoCHelper import read_input_lines
 
-input = read_input_lines("AoC23/Inputs/Day15/inputs.txt")[0]
-
-result = 0
-init_sequence = input.split(",")
-
 
 def compute_hash(string: str) -> int:
     hash = 0
@@ -18,32 +13,36 @@ def compute_hash(string: str) -> int:
     return hash
 
 
-for init in init_sequence:
-    result += compute_hash(init)
+def populate_boxes(init_sequence):
+    boxes = [{} for _ in range(256)]
+    for init in init_sequence:
+        op_idx = re.search(r"=|-", init).start()
+        label = init[:op_idx]
+        operation = init[op_idx]
 
-print(result)
+        label_hash = compute_hash(label)
 
-boxes = [{} for _ in range(256)]
-for init in init_sequence:
-    op_match = re.search(r"=|-", init)
-    op_idx = op_match.start()
-    label = init[:op_idx]
-    operation = init[op_idx]
+        if operation == "-":
+            if label in boxes[label_hash].keys():
+                del boxes[label_hash][label]
+        else:
+            focal_lenght = init[op_idx + 1]
+            boxes[label_hash][label] = focal_lenght
 
-    label_hash = compute_hash(label)
+    return boxes
 
-    if operation == "-":
-        if label in boxes[label_hash].keys():
-            del boxes[label_hash][label]
-    else:
-        focal_lenght = init[op_idx + 1]
-        boxes[label_hash][label] = focal_lenght
 
-result2 = 0
-for idx, box in enumerate(boxes):
-    for idy, label in enumerate(box.keys()):
-        score = (idx + 1) * (idy + 1) * int(box[label])
-        print(score)
-        result2 += score
+if __name__ == "__main__":
+    input = read_input_lines("AoC23/Inputs/Day15/inputs.txt")[0]
+    init_sequence = input.split(",")
 
-print(result2)
+    result = sum(compute_hash(init) for init in init_sequence)
+    assert result == 513214
+    print(f"Part 1: {result}")
+
+    result2 = 0
+    for idx, box in enumerate(populate_boxes(init_sequence)):
+        for idy, label in enumerate(box.keys()):
+            result2 += (idx + 1) * (idy + 1) * int(box[label])
+    assert result2 == 258826
+    print(f"Part 2: {result2}")
