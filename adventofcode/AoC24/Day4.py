ba945f9e2b778252d59ae8922485ac6a1213cae2
Andreas Midjord <amid@baselifescience.com>
2024-12-04 13:52:48 +0100

    Day 4 cleanup

--- adventofcode/AoC24/Day4.py
+++ adventofcode/AoC24/Day4.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from pathlib import Path
 
@@ -5,20 +6,46 @@ from adventofcode.helpers.AoCHelper import (
     read_input_lines,
 )
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 INPUT_FOLDER_PATH = Path("AoC24") / "Inputs" / "Day4"
-
-input_lines = read_input_lines(INPUT_FOLDER_PATH / "input.txt")
-
-print(f"Found {len(input_lines)} input lines of length {len(input_lines[0])}")
-
-PATTERN = r"XMAS"
 
 
 def rotate_matrix(matrix: list[list[str]]) -> list[list[str]]:
+    """Rotate a matrix by 90 degrees clockwise.
+
+    Parameters
+    ----------
+    matrix : list[list[str]]
+        The matrix to rotate.
+
+    Returns
+    -------
+    list[list[str]]
+        The rotated matrix.
+
+    """
     return [list(row) for row in zip(*matrix[::-1])]
 
 
 def get_diagonal(matrix: list[list[str]], index: int) -> list[str]:
+    """Get elements in diagonal row of a matrix.
+
+    Parameters
+    ----------
+    matrix : list[list[str]]
+        The matrix to get the diagonal from.
+    index : int
+        The index of the diagonal. 0 is the main diagonal, positive numbers are
+        above the main diagonal, negative numbers are below the main diagonal.
+
+    Returns
+    -------
+    list[str]
+        The elements in the diagonal row.
+
+    """
     if index == 0:
         return [matrix[i][i] for i in range(len(matrix))]
     if index > 0:
@@ -27,39 +54,33 @@ def get_diagonal(matrix: list[list[str]], index: int) -> list[str]:
         return [matrix[i - index][i] for i in range(len(matrix) + index)]
 
 
-test_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(test_matrix)
-print(rotate_matrix(test_matrix))
+def solve_one(input_file: str) -> int:
+    input_lines = read_input_lines(INPUT_FOLDER_PATH / input_file)
+    pattern = r"XMAS"
 
-for i in range(-2, 3):
-    print(get_diagonal(test_matrix, i))
+    occurences_found = 0
 
-res = 0
+    for _ in range(4):
+        for line in input_lines:
+            matches = re.findall(pattern, "".join(line))
+            occurences_found += len(matches)
 
-for _ in range(4):
-    print("Searching horizontally")
-    for idx, line in enumerate(input_lines):
-        matches = re.findall(PATTERN, "".join(line))
-        res += len(matches)
-        if matches:
-            print(f"Found {len(matches)} matches in line {idx + 1}")
-    print("Searching diagonally")
-    for i in range(-len(input_lines) + 1, len(input_lines)):
-        matches = re.findall(PATTERN, "".join(get_diagonal(input_lines, i)))
-        res += len(matches)
-        if matches:
-            print(f"Found {len(matches)} matches in diagonal {i}")
-    print("Rotating matrix")
-    input_lines = rotate_matrix(input_lines)
+        for i in range(-len(input_lines) + 1, len(input_lines)):
+            matches = re.findall(pattern, "".join(get_diagonal(input_lines, i)))
+            occurences_found += len(matches)
 
-print(f"Found {res} matches in total")
+        input_lines = rotate_matrix(input_lines)
+
+    return occurences_found
 
 
-res = 0
-for i in range(len(input_lines)):
-    for j in range(len(input_lines[i])):
-        if input_lines[i][j] == "A":
-            try:
+def solve_two(input_file: str) -> int:
+    input_lines = read_input_lines(INPUT_FOLDER_PATH / input_file)
+
+    occurences_found = 0
+    for i in range(1, len(input_lines) - 1):
+        for j in range(1, len(input_lines[i]) - 1):
+            if input_lines[i][j] == "A":
                 up_left = input_lines[i - 1][j - 1]
                 up_right = input_lines[i - 1][j + 1]
                 down_left = input_lines[i + 1][j - 1]
@@ -68,10 +89,15 @@ for i in range(len(input_lines)):
                 if (f"{up_left}A{down_right}" in ["SAM", "MAS"]) and (
                     f"{up_right}A{down_left}" in ["SAM", "MAS"]
                 ):
-                    print(f"Found X-MAS at ({i}, {j}).")
-                    res += 1
-            except IndexError:
-                print(f"({i}, {j}) on border - skipping.")
+                    occurences_found += 1
+    return occurences_found
 
 
-print(f"Found {res} matches in total")
+if __name__ == "__main__":
+    res = solve_one("input.txt")
+    assert res == 2591, f"Test failed: got {res}"
+    logger.info(f"Part 1: {res}")
+
+    res = solve_two("input.txt")
+    assert res == 1880, f"Test failed: got {res}"
+    logger.info(f"Part 2: {res}")
