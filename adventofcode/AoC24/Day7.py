0e3b50c4fad398cf0522f53eef8dbe64643ccf01
Andreas Midjord <amid@baselifescience.com>
2024-12-08 00:26:01 +0100

    Day 7 cleanup

--- adventofcode/AoC24/Day7.py
+++ adventofcode/AoC24/Day7.py
@@ -1,10 +1,15 @@
+import logging
+import time
 from pathlib import Path
+
 from adventofcode.helpers.AoCHelper import (
-    read_input_lines,
     extract_numbers_from_line,
     get_all_combinations,
+    read_input_lines,
 )
-import time
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 INPUT_FOLDER_PATH = Path("AoC24") / "Inputs" / "Day7"
 
@@ -21,35 +26,39 @@ def concat(a: int, b: int) -> int:
     return int(str(a) + str(b))
 
 
-start_time = time.time()
+def solve(input_file: str, available_operations: list[str]) -> int:
+
+    input_lines = read_input_lines(INPUT_FOLDER_PATH / input_file)
+
+    result = 0
+
+    for i in input_lines:
+        expected, *inputs = extract_numbers_from_line(i)
+
+        operations = get_all_combinations(available_operations, len(inputs) - 1)
+
+        for op in operations:
+            temp_res = inputs[0]
+            for idx, o in enumerate(op):
+                if o == "+":
+                    temp_res = plus(temp_res, inputs[idx + 1])
+                elif o == "*":
+                    temp_res = multiply(temp_res, inputs[idx + 1])
+                elif o == "||":
+                    temp_res = concat(temp_res, inputs[idx + 1])
+
+            if temp_res == expected:
+                result += expected
+                break
+
+    return result
 
 
-input_lines = read_input_lines(INPUT_FOLDER_PATH / "input.txt")
+if __name__ == "__main__":
+    res = solve("input.txt", ["+", "*"])
+    assert res == 1298300076754, f"Test failed: got {res}"
+    logger.info(f"Part 1: {res}")
 
-result = 0
-
-
-for i in input_lines:
-    expected, *inputs = extract_numbers_from_line(i)
-
-    operations = get_all_combinations(["+", "*", "||"], len(inputs) - 1)
-
-    for op in operations:
-        temp_res = inputs[0]
-        for idx, o in enumerate(op):
-            if o == "+":
-                temp_res = plus(temp_res, inputs[idx + 1])
-            elif o == "*":
-                temp_res = multiply(temp_res, inputs[idx + 1])
-            elif o == "||":
-                temp_res = concat(temp_res, inputs[idx + 1])
-
-        if temp_res == expected:
-            print("Match found with operations:", op)
-            result += expected
-            break
-
-print(result)
-end_time = time.time()
-
-print(f"Duration: {end_time - start_time}")
+    res = solve("input.txt", ["+", "*", "||"])
+    assert res == 248427118972289, f"Test failed: got {res}"
+    logger.info(f"Part 2: {res}")
