39774bdade5509117ecd11a8800fd8496b73d6f0
Andreas Midjord <amid@baselifescience.com>
2025-12-04 08:17:38 +0100

    Solve Day 4 part 2

--- adventofcode/AoC25/Day4.py
+++ adventofcode/AoC25/Day4.py
@@ -8,6 +8,19 @@ path = Path("AoC25") / "Inputs" / "Day4"
 logger = logging.getLogger(__name__)
 
 
+def can_pick_up_roll(entry: str, grid: list[list[str]], i: int, j: int) -> bool:
+    if entry == "@":
+        neighbours = get_neighbours(i, j, grid)
+        roll_neighbours = [n for n in neighbours if n == "@"]
+
+        if len(roll_neighbours) < 4:
+            logger.debug(f"Found roll at index ({i},{j}) to pick up")
+            return True
+        else:
+            return False
+    return False
+
+
 def solve_one(input_file: str) -> int:
     input = read_input_lines(path / input_file)
     grid = [list(i) for i in input]
@@ -16,27 +29,43 @@ def solve_one(input_file: str) -> int:
 
     for i, row in enumerate(grid):
         for j, entry in enumerate(row):
-            if entry == "@":
-                neighbours = get_neighbours(i, j, grid)
-                roll_neighbours = [n for n in neighbours if n == "@"]
-
-                if len(roll_neighbours) < 4:
-                    logger.debug(f"Found roll at index ({i},{j}) to pick up")
-                    res += 1
+            if can_pick_up_roll(entry, grid, i, j):
+                res += 1
 
     return res
 
 
 def solve_two(input_file: str) -> int:
     input = read_input_lines(path / input_file)
+    grid = [list(i) for i in input]
 
     res = 0
+    rolls_removed = 0
+
+    while True:
+        rolls_removed = 0
+        new_grid = []
+        for i, row in enumerate(grid):
+            new_row = []
+            for j, entry in enumerate(row):
+                if can_pick_up_roll(entry, grid, i, j):
+                    rolls_removed += 1
+                    new_row.append(".")
+                else:
+                    new_row.append(entry)
+            new_grid.append(new_row)
+
+        if rolls_removed == 0:
+            break
+
+        res += rolls_removed
+        grid = new_grid
 
     return res
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     res = solve_one("input.txt")
     logger.info(f"Part 1: {res}")
