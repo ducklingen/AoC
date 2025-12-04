94e9f7031c226b29df8cd86a624327fc8dc87953
Andreas Midjord <amid@baselifescience.com>
2025-12-04 08:21:28 +0100

    Refactor day 3

--- adventofcode/AoC25/Day3.py
+++ adventofcode/AoC25/Day3.py
@@ -8,21 +8,34 @@ path = Path("AoC25") / "Inputs" / "Day3"
 logger = logging.getLogger(__name__)
 
 
+def get_max_voltage(battery: str, length: int) -> int:
+    digits = []
+
+    for j in range(length - 1):
+        next_digit = max(int(i) for i in battery[: -(length - j - 1)])
+
+        digits.append(next_digit)
+
+        leading_digit_pos = battery.index(str(next_digit))
+        battery = battery[leading_digit_pos + 1 :]
+
+        logger.debug(
+            f"Found digit {next_digit}, with remaining battery {battery}"
+        )
+
+    digits.append(max(int(i) for i in battery))
+
+    logger.debug(f"Max voltage {''.join(map(str, digits))}")
+    return int("".join(map(str, digits)))
+
+
 def solve_one(input_file: str) -> int:
     input = read_input_lines(path / input_file)
 
     res = 0
 
     for battery in input:
-        leading_digit = max(int(i) for i in battery[:-1])
-        logger.debug(f"Found leading digit {leading_digit}")
-
-        leading_digit_pos = battery.index(str(leading_digit))
-        second_digit = max(int(i) for i in battery[leading_digit_pos + 1 :])
-
-        logger.debug(f"Max voltage {leading_digit}{second_digit}")
-
-        res += leading_digit * 10 + second_digit
+        res += get_max_voltage(battery, 2)
 
     return res
 
@@ -33,24 +46,7 @@ def solve_two(input_file: str) -> int:
     res = 0
 
     for battery in input:
-        digits = []
-
-        for j in range(12 - 1):
-            leading_digit = max(int(i) for i in battery[: -(12 - j - 1)])
-
-            digits.append(str(leading_digit))
-
-            leading_digit_pos = battery.index(str(leading_digit))
-            battery = battery[leading_digit_pos + 1 :]
-
-            logger.debug(
-                f"Found digit {leading_digit}, with remaining battery {battery}"
-            )
-
-        digits.append(str(max(int(i) for i in battery)))
-
-        logger.debug(f"Max voltage {''.join(digits)}")
-        res += int("".join(digits))
+        res += get_max_voltage(battery, 12)
 
     return res
 
@@ -59,7 +55,9 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     res = solve_one("input.txt")
+    assert res == 17031, "Part 1 result does not match expected value"
     logger.info(f"Part 1: {res}")
 
     res = solve_two("input.txt")
+    assert res == 168575096286051, "Part 2 result does not match expected value"
     logger.info(f"Part 2: {res}")
